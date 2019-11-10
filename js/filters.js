d074c090e42ba90747da075da000d4c389504dbd
Egor Pisarev <egor@pisarev.in>
2019-11-10 18:01:07 +0300

    Minor changes

--- js/filters.js
+++ js/filters.js
@@ -1,16 +1,15 @@
-"use strict";
+'use strict';
 var WIDTH_OF_SLIDER = 495;
-var previewImg = document.querySelector(".img-upload__preview");
-var sligerAria = document.querySelector(".img-upload__effect-level ");
-var oldposX, oldleft, selectedFilter;
-
-console.log(sligerAria);
+var previewImg = document.querySelector('.img-upload__preview');
+var oldposX;
+var oldleft;
+var selectedFilter;
 
 function pxToNumber(styleLeft) {
   return parseInt(styleLeft.substr(0, styleLeft.length - 2));
 }
 
-var moveListener = function(event) {
+var moveListener = function (event) {
   var newpos = event.clientX + pxToNumber(oldleft) - oldposX; // - oldpos;
 
   if (newpos < 0) {
@@ -21,27 +20,27 @@ var moveListener = function(event) {
     newpos = 453;
   }
 
-  deepOfEffect.style.left = newpos + "px";
+  deepOfEffect.style.left = newpos + 'px';
 
   previewImg.style.filter = changeFilter(selectedFilter, newpos);
 };
 
-var deepOfEffect = document.querySelector(".effect-level__pin");
-deepOfEffect.style.left = "19px";
+var deepOfEffect = document.querySelector('.effect-level__pin');
+deepOfEffect.style.left = '19px';
 
-deepOfEffect.addEventListener("mousedown", function(event) {
+deepOfEffect.addEventListener('mousedown', function (event) {
   oldposX = event.clientX;
   oldleft = deepOfEffect.style.left;
   selectedFilter = getInputWithValue();
-  document.addEventListener("mousemove", moveListener, false);
+  document.addEventListener('mousemove', moveListener, false);
 });
 
-document.addEventListener("mouseup", function() {
-  document.removeEventListener("mousemove", moveListener, false);
+document.addEventListener('mouseup', function () {
+  document.removeEventListener('mousemove', moveListener, false);
 });
 
 function getInputWithValue() {
-  var inputs = document.querySelectorAll(".effects__radio");
+  var inputs = document.querySelectorAll('.effects__radio');
   var i = inputs.length;
   while (i--) {
     if (inputs[i].checked === true) {
@@ -52,38 +51,38 @@ function getInputWithValue() {
 }
 
 function changeFilter(filterValue, sliderLeft) {
-  if (filterValue === "effect-none") {
-    return "";
-  } else if (filterValue === "effect-chrome") {
+  if (filterValue === 'effect-none') {
+    return '';
+  } else if (filterValue === 'effect-chrome') {
     return filterChrome(sliderLeft);
-  } else if (filterValue === "effect-sepia") {
+  } else if (filterValue === 'effect-sepia') {
     return filterSepia(sliderLeft);
-  } else if (filterValue === "effect-marvin") {
+  } else if (filterValue === 'effect-marvin') {
     return filterMarvin(sliderLeft);
-  } else if (filterValue === "effect-phobos") {
+  } else if (filterValue === 'effect-phobos') {
     return filterBlur(sliderLeft);
-  } else if (filterValue === "effect-heat") {
+  } else if (filterValue === 'effect-heat') {
     return filterBritness(sliderLeft);
   }
   return null;
 }
 
 function filterChrome(sliderLeft) {
-  return "grayscale(" + (sliderLeft / 453).toString() + ")";
+  return 'grayscale(' + (sliderLeft / 453).toString() + ')';
 }
 
 function filterSepia(sliderLeft) {
-  return "sepia(" + (sliderLeft / 453).toString() + ")";
+  return 'sepia(' + (sliderLeft / 453).toString() + ')';
 }
 
 function filterMarvin(sliderLeft) {
-  return "invert(" + Math.round((sliderLeft / 453) * 100).toString() + "%)";
+  return 'invert(' + Math.round((sliderLeft / 453) * 100).toString() + '%)';
 }
 
 function filterBlur(sliderLeft) {
-  return "blur(" + ((sliderLeft / 453) * 3).toString() + "px)";
+  return 'blur(' + ((sliderLeft / 453) * 3).toString() + 'px)';
 }
 
 function filterBritness(sliderLeft) {
-  return "brightness(" + ((sliderLeft / 453) * 2 + 1).toString() + ")";
+  return 'brightness(' + ((sliderLeft / 453) * 2 + 1).toString() + ')';
 }
